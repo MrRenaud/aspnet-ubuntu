3b5739dfecba4fb36656d5dd05f64284c8f58930
Renaud <renaud.dumont@live.fr>
2014-12-11 14:46:38 +0100

    on tente un commit via VS2015

---
+++ WebApplication/src/WebApplication/Models/TodoItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebApplication.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsDone { get; set; }
+
+    }
+}
---
+++ WebApplication/src/WebApplication/Startup.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+using Microsoft.AspNet.Routing;
+
+using Microsoft.Framework.DependencyInjection;
+
+namespace WebApplication
+{
+    public class Startup
+    {
+        public void ConfigureService(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+
+        public void Configure(IApplicationBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+
+            //app.UseWelcomePage();
+
+            app.UseMvc();
+
+            app.Run(context =>
+            {
+                return context.Response.WriteAsync("coucou toi");
+            });
+        }
+    }
+}
